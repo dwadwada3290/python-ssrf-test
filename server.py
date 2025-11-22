920d85d75644e9947788701b80cd5950057dbc3a
dwadwada3290 <mlg68712@gmail.com>
2025-11-22 21:17:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,6 +13,25 @@ def fake_image():
 def not_image():
     # محتوى نصي بدل صورة
     return Response("hello its me", mimetype='image/x-fklsflsd+png')
+
+
+@app.route("/proxy")
+def proxy():
+    # الرابط الذي يريد العميل الوصول إليه
+    external_url = request.args.get("url")
     
+    if not external_url:
+        return "Please provide ?url=", 400
+
+    # إرسال الطلب إلى السيرفر الخارجي
+    external_response = requests.get(external_url)
+
+    # إعادة الردّ للعميل
+    return Response(
+        external_response.content,                     # نفس البيانات
+        status=external_response.status_code,          # نفس الحالة
+        headers={"Content-Type": "image/x-dwdawdwa+png"}          # تغيير الـ Content-Type حسب رغبتك
+    )
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
